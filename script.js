30e5088473af1771a4e2ce880e8d95e3c22f8c91
kodo21 <kodo21hood@gmail.com>
2020-05-08 23:57:44 -0400

    uploading test js and updated html a little bit.

---
+++ script.js
@@ -0,0 +1,38 @@
+var playerHP = 100;
+var playerDmg = 10;
+var enemyHP = 100;
+var enemyDmg = 10;
+var magic = 0;
+console.log(enemyHP);
+enemyHP = incrementHP( enemyDmg, enemyHP);
+console.log(enemyHP);
+enemyHP = decrementHP(enemyDmg, enemyHP);
+console.log(enemyHP);
+function incrementHP(hpToCharacter, character)
+
+{
+return character += hpToCharacter;
+}
+function decrementHP(dmgToCharacter, character)
+
+{
+return character -= dmgToCharacter;
+}
+document.getElementById("test").addEventListener("click", function()
+{
+    enemyHP = incrementHP( enemyDmg, enemyHP);
+    console.log(enemyHP);
+});
+
+document.getElementById("test2").addEventListener("click", function()
+{
+    enemyHP = decrementHP( enemyDmg, enemyHP);
+    console.log(enemyHP);
+});
+function increaseMagic()
+{
+
+    magic++;
+
+}
+document.getElementById("progress").value = 100;
