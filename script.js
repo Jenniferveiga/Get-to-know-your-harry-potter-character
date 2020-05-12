309c7727c6c561233bcffcd2fe52baacaf2b2526
kodo21 <kodo21hood@gmail.com>
2020-05-11 20:50:55 -0400

    setting up example html page and getting api to work. Also building out the game functions (still in progress).

--- script.js
+++ script.js
@@ -18,22 +18,39 @@ function decrementHP(dmgToCharacter, character)
 {
 return character -= dmgToCharacter;
 }
-document.getElementById("test").addEventListener("click", function()
+ document.getElementById("attackPlayer").addEventListener("click", function()
 {
     enemyHP = incrementHP( enemyDmg, enemyHP);
     console.log(enemyHP);
 });
 
-document.getElementById("test2").addEventListener("click", function()
+document.getElementById("healPlayer").addEventListener("click", function()
 {
     enemyHP = decrementHP( enemyDmg, enemyHP);
     console.log(enemyHP);
 });
+document.getElementById("shieldPlayer").addEventListener("click", function()
+{
+    enemyHP = incrementHP( enemyDmg, enemyHP);
+    console.log(enemyHP);
+});
+
+document.getElementById("randomSpellPlayer").addEventListener("click", function()
+{
+    enemyHP = decrementHP( enemyDmg, enemyHP);
+    console.log(enemyHP);
+});
+
+function damageBoss(){}
+function healBoss(){}
+function shieldBoss(){}
+function randomSpellBoss(){}
+
 function increaseMagic()
 {
-
     magic++;
-
 }
-document.getElementById("progress").value = 100;
+//document.getElementById("progress").value = 100;
 //document.getElementById("progress").max = 200;
+//https://www.potterapi.com/v1/characters/?key=$2a$10$H1pmgojMlcHoQBh9QhzYYeYnIfKZtYOxZR2FQ6SH0lNbYzgMEQVme
+//&house=Slytherin
